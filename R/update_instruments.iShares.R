a6abb7469d42f04fd28a4cce024ae1a839376bdf
JustinMShea <justin.shea@gmail.com>
2026-07-11 21:43:09 -0500

    fixed NOTES regarding various URLs

--- R/update_instruments.iShares.R
+++ R/update_instruments.iShares.R
@@ -1,31 +1,31 @@
 #' update iShares and SPDR ETF metadata
-#' 
-#' This will update previously defined iShares or SPDR ETF \code{instrument}s. 
+#'
+#' This will update previously defined iShares or SPDR ETF \code{instrument}s.
 #' Both functions will add attributes for \dQuote{Name}, and \dQuote{FundFamily}
-#' (\dQuote{iShares} or \dQuote{SPDR}). \code{update_instruments.iShares} will 
+#' (\dQuote{iShares} or \dQuote{SPDR}). \code{update_instruments.iShares} will
 #' also add an attribute for \dQuote{MgmtFees}
-#' 
-#' @param Symbols character vector of iShares ETF ticker symbols.  If not 
+#'
+#' @param Symbols character vector of iShares ETF ticker symbols.  If not
 #'   specified, \code{unique(c(ls_funds(), ls_stocks()))} will be used.
-#' @param silent silence the warning that no iShares are defined? 
+#' @param silent silence the warning that no iShares are defined?
 #' @return called for side-effect
 #' @author Garrett See
 #' @seealso \code{update_instruments.yahoo}, \code{update_instruments.TTR},
-#'   \code{twsInstrument:::update_instruments.IB}, 
-#'   \code{update_instruments.instrument}, 
+#'   \code{twsInstrument:::update_instruments.IB},
+#'   \code{update_instruments.instrument},
 #'   \code{\link{update_instruments.morningstar}},
 #'   \code{\link{update_instruments.masterDATA}}
-#' @note \code{update_instruments.SPDR} will probably NOT work on Windows 
-#'   because in the call to \code{download.file} it uses \code{method=curl} 
+#' @note \code{update_instruments.SPDR} will probably NOT work on Windows
+#'   because in the call to \code{download.file} it uses \code{method=curl}
 #'   since it has to download from an https URL scheme.
-#' @references \url{http://us.ishares.com/home.htm}, 
-#'   \url{https://www.spdrs.com/}
+#' @references \url{http://us.ishares.com/home.htm},
+#'   \url{https://www.ssga.com/de/en_gb/intermediary}
 #' @examples
 #' \dontrun{
 #' stock("IWC", currency("USD"))
 #' update_instruments.iShares("IWC")
 #' getInstrument("IWC")
-#' 
+#'
 #' Symbols <- stock(c("SPY", "JNK"), currency("USD"))
 #' update_instruments.SPDR(Symbols)
 #' buildHierarchy(c("SPY", "JNK"), "Name")
@@ -38,9 +38,9 @@ update_instruments.iShares <- function(Symbols, silent=FALSE) {
                 "?assetClassCd=EQ&ticker=&asofDt=", sep="")
   download.file(lnk, destfile=tmp, quiet=TRUE)
   fr <- read.csv(tmp, skip=3, stringsAsFactors=FALSE, header=FALSE)
-  colnames(fr) <- read.delim(text=readLines(tmp, 1), sep=",", header=FALSE, 
+  colnames(fr) <- read.delim(text=readLines(tmp, 1), sep=",", header=FALSE,
                              stringsAsFactors=FALSE)
-  unlink(tmp)  
+  unlink(tmp)
   tickers <- fr[["Ticker"]]
   if (missing(Symbols)) {
     Symbols <- unique(c(ls_funds(), ls_stocks()))
@@ -57,12 +57,12 @@ update_instruments.iShares <- function(Symbols, silent=FALSE) {
     instrument_attr(dat[i, "Ticker"], "MgmtFees", dat[i, "Mgmt Fees"])
     instrument_attr(dat[i, "Ticker"], "Name", dat[i, "iShares Fund Name"])
     instrument_attr(dat[i, "Ticker"], "FundFamily", "iShares")
-  } 
+  }
   invisible(lapply(s, function(x) {
     db <- getInstrument(x)[["defined.by"]]
     db <- if (is.null(db)) {
       "iShares"
-    } else paste(unique(c(strsplit(db, ";")[[1]], 
+    } else paste(unique(c(strsplit(db, ";")[[1]],
               "iShares")), collapse=";")
     instrument_attr(x, "defined.by", db)
     instrument_attr(x, "updated", Sys.time())
@@ -71,7 +71,7 @@ update_instruments.iShares <- function(Symbols, silent=FALSE) {
 }
 
 
-#' @export 
+#' @export
 #' @rdname update_instruments.iShares
 update_instruments.SPDR <- function(Symbols, silent=FALSE) {
   tmp <- tempfile()
@@ -95,10 +95,10 @@ update_instruments.SPDR <- function(Symbols, silent=FALSE) {
   }
   dat <- fr[fr[["TICKER"]] %in% s, ]
   for (i in seq_len(NROW(dat))) {
-    instrument_attr(dat[i, "TICKER"], "Name", 
+    instrument_attr(dat[i, "TICKER"], "Name",
                     gsub("^\\s+|\\s+$", "", dat[i, "NAME"]))
     instrument_attr(dat[i, "TICKER"], "FundFamily", "SPDR")
-    add.identifier(dat[i, "TICKER"], 
+    add.identifier(dat[i, "TICKER"],
                    SPDR=gsub("^\\s+|\\s+$", "", dat[i, "FUND"]))
     add.identifier(dat[i, "TICKER"], CUSIP=gsub("\'", "", fr[i, "CUSIP"]))
   }
--- R/update_instruments.yahoo.R
+++ R/update_instruments.yahoo.R
@@ -2,7 +2,7 @@
 # R (http://r-project.org/) Instrument Class Model
 #
 # Copyright (c) 2009-2012
-# Peter Carl, Dirk Eddelbuettel, Jeffrey Ryan, 
+# Peter Carl, Dirk Eddelbuettel, Jeffrey Ryan,
 # Joshua Ulrich, Brian G. Peterson, and Garrett See
 #
 # This library is distributed under the terms of the GNU Public License (GPL)
@@ -14,9 +14,9 @@
 
 
 #' updates instrument metadata with data from yahoo
-#' 
+#'
 #' Adds/updates information in instrument with data downloaded from yahoo
-#' 
+#'
 #' Although these functions are intended to update the metadata of
 #' previously defined instruments, \code{update_instruments.TTR} will
 #' define the stocks if they do not already exist.
@@ -33,15 +33,14 @@
 #' @param silent silence warnings?
 #' @return called for side-effect
 #' @author Garrett See
-#' @seealso \code{\link{update_instruments.instrument}}, 
+#' @seealso \code{\link{update_instruments.instrument}},
 #'   \code{\link{update_instruments.morningstar}},
 #'   \code{\link{update_instruments.masterDATA}},
 #'   \code{\link[TTR]{stockSymbols}}, \code{\link{stock}}
-#' @references Yahoo! Finance \url{finance.yahoo.com} YahooQuote
-#' \url{http://dirk.eddelbuettel.com/code/yahooquote.html} 
-#' gummy-stuff.org \url{www.gummy-stuff.org/Yahoo-data.htm} 
+#' @references Yahoo! Finance "finance.yahoo.com" and YahooQuote
+#' \url{https://dirk.eddelbuettel.com/code/yahooquote.html}
 #' @examples
-#' \dontrun{	
+#' \dontrun{
 #' 	stock('GS',currency('USD'))
 #'  update_instruments.yahoo('GS')
 #' 	getInstrument('GS')
@@ -56,44 +55,44 @@ update_instruments.yahoo <- function(Symbols=c('stocks','all'), verbose=FALSE ) 
     symkey <- na.omit(symkey)[1]
     if (!is.na(symkey)) {
 	    if (symkey == 'all' || symkey == 'stocks' || is.null(Symbols)){
-            if (symkey == 'all') warning('yahoo can only update stocks.')            
+            if (symkey == 'all') warning('yahoo can only update stocks.')
             Symbols <- unique(c(ls_stocks(), ls_instruments_by('src','yahoo')))
-        } 
+        }
     }
     #make sure it's a vector of instrument names
     if (!is.character(Symbols)) {
-        if (verbose) cat('No stocks found to update.\n') 
-        return(NULL) #stop('Symbols must be a vector of instrument names, or one of "all", "stocks"')    
+        if (verbose) cat('No stocks found to update.\n')
+        return(NULL) #stop('Symbols must be a vector of instrument names, or one of "all", "stocks"')
     }
     yahoo.syms <- Symbols
     for (i in 1:length(Symbols)) {
         tmp_instr <- try(getInstrument(Symbols[i]),silent=TRUE)
-        yahoo.syms[i] <- if (!inherits(tmp_instr, 'try-error') 
-                            && !is.null(tmp_instr$src) 
-                            && any(names(tmp_instr$src) == 'name')) 
+        yahoo.syms[i] <- if (!inherits(tmp_instr, 'try-error')
+                            && !is.null(tmp_instr$src)
+                            && any(names(tmp_instr$src) == 'name'))
                          { tmp_instr$src$name } else Symbols[i]
     }
     yahoo.syms <- paste(yahoo.syms, collapse=";")
-	if (is.null(yahoo.syms) || length(yahoo.syms) == 0) 
+	if (is.null(yahoo.syms) || length(yahoo.syms) == 0)
         stop('error with symbol names; no Symbols supplied?')
     yahooStuff <- getQuote(yahoo.syms, src="yahoo",
-					  what=yahooQF(c("Name", 
+					  what=yahooQF(c("Name",
                         "Stock Exchange",
 					    "Market Capitalization",
-					    "Average Daily Volume", 
-                        "Earnings/Share", 
-					    "EPS Estimate Current Year", 
-					    "EPS Estimate Next Year", 
-            			"Book Value", "EBITDA",	
-                        "52-week Range")))  
-#    sym.length <- length(unlist(strsplit(Symbols,";")))    	
+					    "Average Daily Volume",
+                        "Earnings/Share",
+					    "EPS Estimate Current Year",
+					    "EPS Estimate Next Year",
+            			"Book Value", "EBITDA",
+                        "52-week Range")))
+#    sym.length <- length(unlist(strsplit(Symbols,";")))
     #see yahooQF for available whats
     for (i in 1:length(Symbols)) {
         noNA <- function(x) {
             if (x == 'N/A' || is.na(x)) {NULL} else {x}
         }
         instr <- getInstrument(Symbols[i])
-		#Only update stocks from yahoo		
+		#Only update stocks from yahoo
 		if (inherits(instr,'stock') || any(instr$src == 'yahoo')) {
 		    instr$name=noNA(as.character(yahooStuff[i,2]))
 			instr$exchange=noNA(as.character(yahooStuff[i,3]))
@@ -108,26 +107,26 @@ update_instruments.yahoo <- function(Symbols=c('stocks','all'), verbose=FALSE ) 
 	#		instr$IB=twsSTK(as.character(symdesc[i,1]),'SMART'),
 
             tclass <- unique(c(class(instr),'instrument'))
-            class(instr) <- tclass        
+            class(instr) <- tclass
             db <- instr$defined.by
 		    if (!is.null(db)) {
 		        db <- unlist(strsplit(db,";"))
 		        db <- rev(unique(c("yahoo", rev(db))))
-		        db <- paste(db,collapse=";") 
+		        db <- paste(db,collapse=";")
 		    } else db <- "yahoo"
-			instr$defined.by=db 
+			instr$defined.by=db
 		    instr$updated=Sys.time()
-            
+
 		    assign(Symbols[i], instr, pos=.instrument)
 		}
-    }        
+    }
     Symbols
 }
 
 #' @export
 #' @rdname update_instruments.yahoo
-update_instruments.TTR <- function(Symbols = c("stocks", "all"), 
-                                   exchange=c("AMEX","NASDAQ","NYSE"), 
+update_instruments.TTR <- function(Symbols = c("stocks", "all"),
+                                   exchange=c("AMEX","NASDAQ","NYSE"),
                                    silent=FALSE) {
     if (!suppressWarnings(is.currency.name("USD"))) currency("USD")
     df <- stockSymbols(exchange=exchange)
@@ -137,8 +136,8 @@ update_instruments.TTR <- function(Symbols = c("stocks", "all"),
     df <- df[df[["Symbol"]] %in% Symbols, ]
     if (nrow(df) == 0) {
         if (!isTRUE(silent)) {
-            warning(paste(paste(Symbols,collapse=","), 
-                          "not found among those listed on", 
+            warning(paste(paste(Symbols,collapse=","),
+                          "not found among those listed on",
                           paste(exchange,collapse=", ")))
         }
         return(invisible(NULL))
@@ -146,18 +145,18 @@ update_instruments.TTR <- function(Symbols = c("stocks", "all"),
     if (!isTRUE(silent)) {
         cat('defining stocks...\n')
     }
-    symout <- NULL    
+    symout <- NULL
     for (i in 1:nrow(df)) {
         primary_id <- as.character(df$Symbol[i])
-        instr <- try(getInstrument(primary_id, silent = TRUE), silent = TRUE) 
+        instr <- try(getInstrument(primary_id, silent = TRUE), silent = TRUE)
         args <- list()
         arg <- as.list(df[i, ])
         arg$defined.by <- 'TTR'
         if (is.instrument(instr) && !inherits(instr, 'stock')) {
             #make a unique primary_id
-            primary_id <- make.names(c(instr$primary_id, 
+            primary_id <- make.names(c(instr$primary_id,
                                     ls_instruments()),unique=TRUE)[-match(ls_instruments(),
-                                        make.names(c(instr$primary_id, ls_instruments()),unique=TRUE))]            
+                                        make.names(c(instr$primary_id, ls_instruments()),unique=TRUE))]
             warning(paste("instrument",instr$primary_id,
                           "is already defined, but not as stock.",
                           "A new instrument", primary_id ,"will be created"))
@@ -166,7 +165,7 @@ update_instruments.TTR <- function(Symbols = c("stocks", "all"),
             if (!is.null(db)) {
                 db <- unlist(strsplit(db,";"))
                 db <- rev(unique(c("TTR", rev(db))))
-                db <- paste(db,collapse=";") 
+                db <- paste(db,collapse=";")
             } else db <- "TTR"
             arg$defined.by <- db
             for (j in 1:length(arg)) {
@@ -180,7 +179,7 @@ update_instruments.TTR <- function(Symbols = c("stocks", "all"),
         arg$updated <- Sys.time()
         symout <- c(symout, do.call("stock", arg))
     }
-    symout 
+    symout
 }
 
 
@@ -188,43 +187,43 @@ update_instruments.TTR <- function(Symbols = c("stocks", "all"),
 #'
 #' Uses the masterDATA.com list of ETFs and ETNs to update previously defined
 #' instruments.
-#' 
+#'
 #' \code{update_instruments.md} is an alias.
-#' 
+#'
 #' MasterDATA classifies each ETF into one of six Fund.Types.  From their
 #' website:
-#' 
-#' US Equity ETF: All constituents trade on a US exchange. Both ProShares and 
-#' Rydex sponsor ETFs with the objective of achieving the performance (or a 
-#' multiple of the performance) of several major US stock indexes. These ETFs 
-#' currently are included in this category despite the fact that their 
+#'
+#' US Equity ETF: All constituents trade on a US exchange. Both ProShares and
+#' Rydex sponsor ETFs with the objective of achieving the performance (or a
+#' multiple of the performance) of several major US stock indexes. These ETFs
+#' currently are included in this category despite the fact that their
 #' constituent lists are generally not limited to US stocks.
-#' 
-#' Global Equity ETF: One or more of the constituents do not trade on a US 
+#'
+#' Global Equity ETF: One or more of the constituents do not trade on a US
 #' Exchange.
-#' 
-#' Fixed Income ETF:  The constituent list contains government and / or 
-#' corporate debt instruments. ETFs with this classification will not be 
+#'
+#' Fixed Income ETF:  The constituent list contains government and / or
+#' corporate debt instruments. ETFs with this classification will not be
 #' considered for inclusion in MasterDATA's index / ETF compilation list.
-#' 
-#' Commodity Based ETF:  This classification of ETF has no constituents but is 
-#' structured to reflect the valuation of a commodity such as gold, silver, oil 
-#' or interest rates. ETFs with this classification will not be considered for 
+#'
+#' Commodity Based ETF:  This classification of ETF has no constituents but is
+#' structured to reflect the valuation of a commodity such as gold, silver, oil
+#' or interest rates. ETFs with this classification will not be considered for
 #' inclusion in MasterDATA's index / ETF compilation list.
-#' 
-#' Exchange Traded Notes: A type of unsecured, unsubordinated debt security that 
-#' was first issued by Barclays Bank PLC. The purpose of ETNs is to create a 
-#' type of security that combines both the aspects of bonds and exchange traded 
+#'
+#' Exchange Traded Notes: A type of unsecured, unsubordinated debt security that
+#' was first issued by Barclays Bank PLC. The purpose of ETNs is to create a
+#' type of security that combines both the aspects of bonds and exchange traded
 #' funds (ETF). Similar to ETFs, ETNs are traded on a major exchange.
-#' 
+#'
 #' @param Symbols character vector of Symbols of ETFs
 #' @param silent silence warnings?
-#' @return called for side-effect. Each ETF that is updated will be given 
+#' @return called for side-effect. Each ETF that is updated will be given
 #'   instrument attributes of \dQuote{Name} and \dQuote{Fund.Type}
 #' @author Garrett See
-#' @references \url{http://masterDATA.com} 
+#' @references \url{http://masterDATA.com}
 #' (\url{http://www.masterdata.com/helpfiles/ETF_List_Downloads/AllTypes.csv})
-#' @seealso \code{\link{update_instruments.yahoo}}, 
+#' @seealso \code{\link{update_instruments.yahoo}},
 #'   \code{\link{update_instruments.instrument}}
 #' @examples
 #' \dontrun{
@@ -234,13 +233,13 @@ update_instruments.TTR <- function(Symbols = c("stocks", "all"),
 #' }
 #' @export
 update_instruments.masterDATA <- function(Symbols, silent=FALSE) {
-    x <- read.csv("http://www.masterdata.com/helpfiles/ETF_List_Downloads/AllTypes.csv", 
+    x <- read.csv("http://www.masterdata.com/helpfiles/ETF_List_Downloads/AllTypes.csv",
                   stringsAsFactors=FALSE)
     if (missing(Symbols)) Symbols <- unique(c(ls_funds(), ls_stocks()))
     s <- Symbols[Symbols %in% x[["Symbol"]]]
     if (length(s) > 0) {
         # only those that inherit stock or fund
-        s <- s[sapply(lapply(s, getInstrument, type=c("stock", "fund"), 
+        s <- s[sapply(lapply(s, getInstrument, type=c("stock", "fund"),
                              silent = TRUE), is.instrument)]
     }
     if (length(s) == 0) {
@@ -249,7 +248,7 @@ update_instruments.masterDATA <- function(Symbols, silent=FALSE) {
         }
         return(invisible())
     }
-    x <- x[!duplicated(x[["Symbol"]]), ]  
+    x <- x[!duplicated(x[["Symbol"]]), ]
     x <- x[x[["Symbol"]] %in% s, ]
     for (i in seq_len(NROW(x))) {
         instrument_attr(x[["Symbol"]][i], "Name", x[["Name"]][i])
@@ -257,7 +256,7 @@ update_instruments.masterDATA <- function(Symbols, silent=FALSE) {
         db <- getInstrument(x[["Symbol"]][i])[["defined.by"]]
         db <- if (is.null(db)) {
             "masterDATA"
-        } else paste(unique(c(strsplit(db, ";")[[1]], "masterDATA")), 
+        } else paste(unique(c(strsplit(db, ";")[[1]], "masterDATA")),
                  collapse = ";")
         instrument_attr(x[["Symbol"]][i], "defined.by", db)
         instrument_attr(x[["Symbol"]][i], "updated", Sys.time())
@@ -273,37 +272,37 @@ update_instruments.md <- update_instruments.masterDATA
 #' Update instruments with metadata from another instrument.
 #'
 #' Update instruments with metadata from another instrument.
-#' 
-#' By default, only attributes that have a value of \code{""} will be given a 
+#'
+#' By default, only attributes that have a value of \code{""} will be given a
 #' new value.
 #'
 #' If \code{create.new} is \code{TRUE}, then if there are attributes in
-#' \code{source_id} that are not in the \code{Symbols}' instrument, those 
-#' attributes will be copied to the updated instruments unless they are in 
+#' \code{source_id} that are not in the \code{Symbols}' instrument, those
+#' attributes will be copied to the updated instruments unless they are in
 #' \code{ignore}.
-#' 
+#'
 #' @param Symbols charcter vector of primary_ids or other instrument identifiers.
 #' of instruments to be updated.  Alternatively, \code{Symbols} can be an
 #' \code{instrument} or list of \code{instrument}s.
 #' @param source_id The primary_id (or other identifier) of an instrument, or
 #' an instrument.  The \code{source_id} instrument will be used to update the
 #' metadata of \code{Symbols}' instruments.
-#' @param create.new If FALSE (Default), only attributes that exist but have 
+#' @param create.new If FALSE (Default), only attributes that exist but have
 #' empty values will be updated.  If TRUE, new attributes will be created if
 #' \code{source_id} has them, but the \code{Symbols} do not.
 #' @param ignore vector of names of instrument attributes that should not be
 #' copied to the updated instruments.
 #' @param assign_i TRUE/FALSE. If TRUE, the updated instruments will be assigned
-#' back into the instrument environment.  If FALSE, a list of updated 
+#' back into the instrument environment.  If FALSE, a list of updated
 #' instruments will be returned
 #' @return if \code{isTRUE(assign_i)} a vector of primary_ids of the instruments
 #' that were upated.  Otherwise, a list of updated instrument objects.
 #' @author Garrett See
-#' @seealso \code{\link{update_instruments.yahoo}}, 
+#' @seealso \code{\link{update_instruments.yahoo}},
 #' \code{\link{all.equal.instrument}}
 #' @note one way to overwrite attributes of one instrument with those of another
-#' is to first set equal to \code{""} those attributes that you want to 
-#' overwrite, then use \code{update_instruments.instrument} to copy the 
+#' is to first set equal to \code{""} those attributes that you want to
+#' overwrite, then use \code{update_instruments.instrument} to copy the
 #' attributes.
 #' @examples
 #' \dontrun{
@@ -311,8 +310,8 @@ update_instruments.md <- update_instruments.masterDATA
 #' currency("USD")
 #' synthetic("SPX", "USD", identifiers=list(yahoo="GSPC"),
 #'           tick_size=0.01,
-#'          liquidHours="T08:30:00/T15:00:00", 
-#'          extraField='something else', 
+#'          liquidHours="T08:30:00/T15:00:00",
+#'          extraField='something else',
 #'          assign_i=TRUE)
 #' stock("SPY", "USD", liquidHours="", assign_i=TRUE)
 #' all.equal(getInstrument("SPX"), getInstrument("SPY"))
@@ -321,18 +320,18 @@ update_instruments.md <- update_instruments.masterDATA
 #' ## Only attributes that == "" are updated by default
 #' update_instruments.instrument("SPY", "SPX", assign_i=FALSE) #liquidHours
 #' update_instruments.instrument("SPY", "SPX", create.new=TRUE,
-#'                               ignore=c("identifiers", "type"), 
+#'                               ignore=c("identifiers", "type"),
 #'                               assign_i=FALSE)
 #' # Although you probably do NOT want to, this will
 #' # copy everything new -- including identifiers and type!
-#' update_instruments.instrument("SPY", "SPX", create.new=TRUE, ignore=NULL, 
-#'                               assign_i=FALSE) 
+#' update_instruments.instrument("SPY", "SPX", create.new=TRUE, ignore=NULL,
+#'                               assign_i=FALSE)
 #' }
 #' @export
 update_instruments.instrument <- function(Symbols, source_id, create.new=FALSE,
                                           ignore="identifiers", assign_i=TRUE) {
-    r <- if (is.instrument(source_id)) { 
-        source_id 
+    r <- if (is.instrument(source_id)) {
+        source_id
     } else getInstrument(source_id)
     if (!is.instrument(r)) {
         stop('source_id is neither an instrument nor the name of an instrument')
@@ -345,12 +344,12 @@ update_instruments.instrument <- function(Symbols, source_id, create.new=FALSE,
             getInstrument(s, silent=TRUE)
         } else s
         if (!is.instrument(si)) {
-            warning(paste('could not find instrument"', s, '"Skipping...')) 
+            warning(paste('could not find instrument"', s, '"Skipping...'))
             return(NULL)
         }
         all.empty <- do.call(c, lapply(si, function(x) all(x == "")))
         all.empty <- all.empty[!names(all.empty) %in% ignore]
-        
+
         names.empty <- names(all.empty[all.empty])
         for (n in names.empty) {
             if (!is.null(r[[n]])) {
@@ -370,15 +369,15 @@ update_instruments.instrument <- function(Symbols, source_id, create.new=FALSE,
         if (!is.null(db)) {
             db <- unlist(strsplit(db,";"))
             db <- rev(unique(c(r$primary_id, rev(db))))
-            db <- paste(db,collapse=";") 
+            db <- paste(db,collapse=";")
         } else db <- r$primary_id
-        si$defined.by <- db 
+        si$defined.by <- db
         si$updated <- Sys.time()
         si
     })
     if (isTRUE(assign_i)) {
         invisible(lapply(out, function(x) {
-            if (!is.null(x)) assign(x$primary_id, x, 
+            if (!is.null(x)) assign(x$primary_id, x,
                                     pos=.instrument)
         }))
     } else return(out)
